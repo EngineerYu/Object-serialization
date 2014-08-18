19dd6f7f557b32acb106c7f5ca0fd1f8649184f2
ZhenFan <zhenfan@ZhenFandeMacBook-Pro.local>
2014-08-18 14:10:56 +0800

    Initial Commit

---
+++ Save/ZYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZYAppDelegate.h
+//  Save
+//
+//  Created by ZhenFan on 14-8-18.
+//  Copyright (c) 2014年 ZhenFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Save/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Save
+//
+//  Created by ZhenFan on 14-8-18.
+//  Copyright (c) 2014年 ZhenFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZYAppDelegate class]));
+    }
+}
